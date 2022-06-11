c3fb53b23dc6aebf83c86e328481386c60fcfc6c
paragip <pal.paragi@gmail.com>
2022-06-11 18:02:28 +0200

    reading user input - added check if the year was correct (not negative age, not too high age, not string instead of numbers

--- src/readingUserInput.java
+++ src/readingUserInput.java
@@ -6,17 +6,26 @@ public class readingUserInput {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt(); // int bekérése
-        scanner.nextLine(); // fontos, enélkül nem adna időt a követkeő név beírására, csak továbbugrana rajta
 
-        System.out.println("Enter your name: "); // bekéri az adatot
-        String name = scanner.nextLine(); // String bekérése
+        boolean hasNextInt = scanner.hasNextInt(); // ahhoz kell, hogy ellenőrizzük hogy az évszámnál valóban számokat kaptunk-e
 
-        System.out.println("Your name is " + name); // kiírja a bekért adatot
+        if (hasNextInt){ // csak akkor hajtjuk végre a követkeőket, ha valóban számot kaptunk az évszámnál, nem stringet
+            int yearOfBirth = scanner.nextInt(); // int bekérése
+            scanner.nextLine(); // fontos, enélkül nem adna időt a követkeő név beírására, csak továbbugrana rajta
 
-        int age = 2022 - yearOfBirth;
-        System.out.println("Your age is " + age + " year");
+            System.out.println("Enter your name: "); // bekéri az adatot
+            String name = scanner.nextLine(); // String bekérése
 
+            int age = 2022 - yearOfBirth;
+
+            if (age > 0 && age < 100) {
+                System.out.println("Your name is " + name + " and your age is " + age + " years old"); // kiírja a bekért adatot
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
         scanner.close();
     }
 
